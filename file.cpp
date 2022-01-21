9e2a9fbcc1ced6835feaaf1c388de29259a2cb34
Gonçalo Nunes <73646319+Goncalo-Palma-Nunes@users.noreply.github.com>
2022-01-21 14:19:23 -0500

    Final version

--- file.cpp
+++ file.cpp
@@ -1,6 +1,14 @@
+/*
+ * Authors: Gonçalo Nunes, Mateus Pinho
+ * 
+ * Description: Program to compute if a graph represents a geneologic tree,
+ * where each vertex has, at most, 2 parents. It then finds all lowest common
+ * ancestors for two given vertexes in the graph
+ * 
+ */
+
+
 #include <vector>
-// // #include <list>
-#include <forward_list>
 #include <queue>
 #include "stdio.h"
 
@@ -12,20 +20,25 @@
 #define BLACK 3
 #define RED 4
 #define BLUE 5
-#define NIL -1
+#define RED_PATH 6
 
 
 /* Represents a graph */
 struct graph {
-    // std::vector<std::list<int>> adjList;
     std::vector<std::vector<int>> adjMatrix;
     size_t numberOfVertices = 0;
 };
 
-graph gt;  // Transpose graph
-std::vector<char> colour;
 
+/* Transpose of the input graph, as the problem does not require the original
+ * graph to be solved */
+graph gt;
 
+/* Colour vector to use in the Deph-First-Search and Breadth-First-Search algorithms */
+std::vector<int> colour;
+
+/* Prints the graph representation in an adjacency list representation
+ * of the graph */
 void printTable() {
 
     for (size_t v = 0; v < gt.numberOfVertices; v++) {
@@ -38,13 +51,12 @@ void printTable() {
 }
 
 void initializeGraphs(int numVertexes) {
-    //TODO maybe resize instead of new one?
-    gt.adjMatrix = std::vector<std::vector<int>>(numVertexes, std::vector<int>(MAX_NUMBER_OF_ANCESTORS, 0));
+    gt.adjMatrix = std::vector<std::vector<int>>(numVertexes);
     gt.numberOfVertices = numVertexes;
     colour.resize(numVertexes, WHITE);
 }
 
-void BFS(graph t, std::vector<char> &colour, int v) {
+void BFS(graph t, std::vector<int> &colour, int v) {
 
     /* Initialize queue */
     auto q = std::queue<int>();
@@ -57,10 +69,7 @@ void BFS(graph t, std::vector<char> &colour, int v) {
         
         /* Iterate over adjacency list */
         for (int x: gt.adjMatrix[u - 1]) {
-            if (x == 0) {
-                /* Reached end of adjacent vertexes for u */
-                continue;
-            }
+
             if (colour[x - 1] == WHITE) {
                 /* Hasn't been explored yet */
                 colour[x - 1] = GRAY;
@@ -71,11 +80,12 @@ void BFS(graph t, std::vector<char> &colour, int v) {
     }
 }
 
-void BFS2(graph t, std::vector<char> &colour, int v) {
-
-    /* Auxiliary vectors */
-    // // // auto lineage = std::vector<int>(gt.numberOfVertices, NIL);
-    auto lineage = std::vector<bool>(gt.numberOfVertices, false);
+void BFS2(graph t, std::vector<int> &colour, int v) {
+    /*
+     * We define as a red path any path that starts on a vertex t, where
+     * t is a lowest common ancestor for v and u and all other other vertexes
+     * on that path are ancestors of t (descendants on the transpose graph)
+     */
 
     /* Initialize queue */
     auto q = std::queue<int>();
@@ -84,8 +94,6 @@ void BFS2(graph t, std::vector<char> &colour, int v) {
     /* Check if v is itself a LCA */
     if (colour[v - 1] == BLACK) {
         colour[v - 1] = RED;
-        // // lineage[v - 1] = RED;
-        lineage[v - 1] = true;
     }
     else {
         colour[v - 1] = BLUE;
@@ -98,33 +106,34 @@ void BFS2(graph t, std::vector<char> &colour, int v) {
 
         /* Iterate over adjacency list */
         for (int x: gt.adjMatrix[u - 1]) {
-            // // // if (colour[x - 1] == RED && lineage[u - 1] == RED) {
-            if (x == 0) {
-                /* Reached end of adjacent vertexes for u */
-                continue;
-            }
-            if (colour[x - 1] == RED && lineage[u - 1] == true) {
-                /* If vertex X is actually an ancestor of another LCA */
-                colour[x - 1] = BLUE;
-                // // // lineage[x - 1] = RED;
-                lineage[x - 1] = true;
+
+            if (colour[x - 1] == RED && (colour[u - 1] == RED || colour[u - 1] == RED_PATH)) {
+                /* If vertex X is actually an ancestor of another LCA candidate */
+                colour[x - 1] = RED_PATH;
             }
             if (colour[x - 1] == WHITE) {
-                /* Hasn't been explored yet */
+                /* Hasn't been explored yet and isn't an intersection
+                 * point */
                 colour[x - 1] = BLUE;
                 q.push(x);
             }
+            if (colour[x - 1] == BLUE && (colour[u - 1] == RED || colour[u - 1] == RED_PATH)) {
+                /* If it is not an intersection point/LCA candidate and is
+                 * part of a red path (is the ancestor of, at least, 1 LCA
+                 * candidate) */
+                colour[x - 1] = RED_PATH;
+            }
             else if (colour[x - 1] == BLACK) {
+                /* Vertex X is an LCA candidate */
                 q.push(x);
-                // // if (lineage[u - 1] == NIL) {
-                if (lineage[u - 1] == false) {
+
+                if (colour[u - 1] != RED && colour[u - 1] != RED_PATH) {
+                    /* Wasn't discovered through a red path */
                     colour[x - 1] = RED;
                 }
                 else {
-                    colour[x - 1] = BLUE;
+                    colour[x - 1] = RED_PATH;
                 }
-                // // // lineage[x - 1] = RED;
-                lineage[x - 1] = true;
 
             }
         }
@@ -134,14 +143,11 @@ void BFS2(graph t, std::vector<char> &colour, int v) {
 int findAllLCA(int v1, int v2, std::vector<int> &LCAs) {
     // Find all LCA of v1 and v2
 
-    // // auto colour = std::vector<int>(gt.numberOfVertices, WHITE);
     size_t size = colour.size();
     for (size_t j = 0; j < size; j++) {
         /* Repaint colour vector */
         colour[j] = WHITE;
     }
-
-    /* RE-SET COLOUR */
 
     /* First BFS */
     BFS(gt, colour, v1);
@@ -149,7 +155,8 @@ int findAllLCA(int v1, int v2, std::vector<int> &LCAs) {
     /* Second BFS */
     BFS2(gt, colour, v2);
 
-    // // size_t size = colour.size();
+    /* Populate array with the computed Lowest Common Ancestors for vertexes
+     * v1 and v2 */
     for (size_t i = 0; i < size; i++) {
         if (colour[i] == RED) {
             LCAs.push_back(i + 1);
@@ -166,7 +173,7 @@ void printLCAs(std::vector<int> &LCA) {
     printf("\n");
 }
 
-int processInput(std::vector<int> &firstLine) {
+void processInput(std::vector<int> &firstLine) {
     size_t numVertexes, numEdges;
     int v1, v2;
 
@@ -174,8 +181,6 @@ int processInput(std::vector<int> &firstLine) {
     scanf("%d%d", &v1, &v2);
     firstLine[0] = v1;
     firstLine[1] = v2;
-    /*firstLine.push_back(v1);
-    firstLine.push_back(v2);*/
 
     /* Read number of vertexes and edges of the graph */
     scanf("%ld%ld", &numVertexes, &numEdges);
@@ -188,23 +193,13 @@ int processInput(std::vector<int> &firstLine) {
         scanf("%d%d", &v1, &v2);
         
         /* Add edge to graph */
-        if (gt.adjMatrix[v2 - 1][0] == 0) {
-            gt.adjMatrix[v2 - 1][0] = v1;
-        } 
-        else if (gt.adjMatrix[v2 - 1][1] == 0) {
-            gt.adjMatrix[v2 - 1][1] = v1;
-        }
-        else {
-            /* Has more than two progenitors */
-            return FALSE;
-        }
+        gt.adjMatrix[v2 - 1].push_back(v1);
 
     }
-    return TRUE;
 }
 
 
-int DFSVisit(graph &g, std::vector<char> &colour, int v) {
+int DFSVisit(graph &g, std::vector<int> &colour, int v) {
     int cycle = FALSE;
     colour[v - 1] = GRAY;
 
@@ -231,9 +226,6 @@ int DFSVisit(graph &g, std::vector<char> &colour, int v) {
 
 int DFSCycleDetection() {
 
-    /* Setup DFS */
-    // // // // std::vector<int> colour = std::vector<int>(gt.numberOfVertices, WHITE);
-    
     /* DFS Main cycle */
     for (size_t v = 0; v < gt.numberOfVertices; v++) {
         if (colour[v] == WHITE) {
@@ -248,12 +240,18 @@ int DFSCycleDetection() {
 
 int isGeneologicTree(std::vector<int> &firstLine) {
 
-    int lessThanThreeParents = TRUE;
 
-    lessThanThreeParents = processInput(firstLine);
+    processInput(firstLine);
 
-    if (lessThanThreeParents == FALSE) {
-        return FALSE;
+    size_t size = gt.adjMatrix.size();
+    for (size_t i = 0; i < size; i++) {
+
+        /* Go through adjacency list for each vertex. If any vertex
+         * has an adjacency with more than MAX_NUMBER_OF_ANCESTORS, then it
+         * cannot be a valid geneologic tree */
+        if (gt.adjMatrix[i].size() > MAX_NUMBER_OF_ANCESTORS) {
+            return FALSE;
+        }
     }
 
     /* Check for cycles */
@@ -279,7 +277,6 @@ int main() {
 
     if (isTree == FALSE) {
         printf("0\n");
-        return 0;
     }
     else {
         if ((hasLCA = findAllLCA(firstLine[0], firstLine[1], LCA))) {
@@ -288,7 +285,6 @@ int main() {
         else {
             printf("-\n");
         }
-        return 0;
     }
     return 0;
 }
